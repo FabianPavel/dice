6b1790bd23e0541f4ebf4e654a59fb0049d2fd63
FabianPavel <pavel.fabian05@gmail.com>
2022-11-05 14:22:25 +0100

    deleted min and max functions, added butto for upgrades, added function for upgrades, after 3 rounds the stats resets and tell if you win or lose, fixed bug with dices on certain width of screen

--- js/hra.js
+++ js/hra.js
@@ -3,18 +3,32 @@ const dice2 = document.getElementById("dice2");
 const button = document.getElementById("play");
 const result = document.getElementById("result");
 const result2 = document.getElementById("result2");
+const upgrade = document.getElementById("upgrade");
 
 let turn = [];
 let turn2 = [];
 let timer = false;
 let turns = [];
 let turns2 = [];
+let upgrades = 0;
+let wins = 0;
+let points = 0;
+let price = 1;
 
 function animation(){
-    turn = Math.ceil(Math.random() * 6);
-    dice.src = `img/kostka${turn}.png`;
-    turn2 = Math.ceil(Math.random() * 6);
-    dice2.src = `img/kostka${turn2}.png`;
+    if (upgrades === 0) {
+        turn = Math.ceil(Math.random() * 6);
+        dice.src = `img/kostka${turn}.png`;
+        turn2 = Math.ceil(Math.random() * 6);
+        dice2.src = `img/kostka${turn2}.png`;
+    }
+    else if (upgrades === 1){
+        turn = Math.ceil(Math.random() * 5);
+        turn ++;
+        dice.src = `img/kostka${turn}.png`;
+        turn2 = Math.ceil(Math.random() * 6);
+        dice2.src = `img/kostka${turn2}.png`;
+    }
 }
 
 
@@ -30,9 +44,31 @@ button.addEventListener(`click`, function() {
         turns.push(turn);
         turns2.push(turn2);
 
+        if(turns.length === 3) {
+            if (sum() > sum2()) {
+                wins++;
+                points++;
+                console.log("you win");
+            } else {
+                console.log("you lost");
+            }
+            turn = [];
+            turn2 = [];
+            turns = [];
+            turns2 = [];
+        }
+
         result.innerHTML = statistic();
         result2.innerHTML = statistic2();
-        //console.log(`${turns.length}`);
+    }
+})
+
+upgrade.addEventListener(`click`, () => {
+    if(points === price){
+        upgrades ++;
+        points -= price;
+        result.innerHTML = statistic();
+        result2.innerHTML = statistic2();
     }
 })
 
@@ -46,30 +82,12 @@ function sum(){
 
 function sum2(){
     let s = 0;
-    for(let i = 0; i < turns.length; i++){
-        s += turns[i];
+    for(let i = 0; i < turns2.length; i++){
+        s += turns2[i];
     }
     return s;
 }
 
-function max(){
-    let m = 0;
-    for(let i = 0; i < turns.length; i++){
-        if(m < turns[i]){
-        m = turns[i];
-        }
-    }
-    return m;
-}
-function min(){
-    let m = 6;
-    for(let i = 0; i < turns.length; i++){
-        if(m > turns[i]){
-        m = turns[i];
-        }
-    }
-    return m;
-}
 
 function statistic() {
     let print = `<h3>STATISTICS</h3>`;
@@ -78,8 +96,8 @@ function statistic() {
     print += `<p>Your throws were: ${turns}</p>`;
     print += `<p>Summary of your throws is: ${sum()}</p>`;
     print += `<p>Average value of throw is: ${(sum() / turns.length).toFixed(2)}</p>`;
-    print += `<p>Minimum: ${min()}</p>`;
-    print += `<p>Maximum: ${max()}</p>`;
+    print += `<p>You've beaten your enemy ${wins} times<p>`;
+    print += `<p>You have ${points} win points <p>`;
     return print;
 }
 
@@ -89,8 +107,6 @@ function statistic2(){
     print2 += `<p>Throws: ${turns.length} </p>`;
     print2 += `<p>Your throws were: ${turns2}</p>`;
     print2 += `<p>Summary of your throws is: ${sum2()}</p>`;
-    print2 += `<p>Average value of throw is: ${(sum() / turns.length).toFixed(2)}</p>`;
-    print2 += `<p>Minimum: ${min()}</p>`;
-    print2 += `<p>Maximum: ${max()}</p>`;
+    print2 += `<p>Average value of throw is: ${(sum2() / turns2.length).toFixed(2)}</p>`;
     return print2;
 }
